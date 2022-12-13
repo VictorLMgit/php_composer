0b2529010ffa4d06ac7cbc5cbb7a9fa4edf72d13
Victor Lima <victorlimacar@gmail.com>
2022-12-13 08:36:49 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+require 'vendor/autoload.php';
+use \GuzzleHttp\Client;
+use src\BuscadorDeCursos\Buscador;
+use \Symfony\Component\DomCrawler\Crawler;
+
+$client = new Client(['base_uri' => 'https://www.alura.com.br/']);
+$crawler = new Crawler();
+$buscador = new Buscador($client, $crawler);
+$cursos = $buscador->buscar("cursos-online-programacao/php");
+
+print_r($cursos);
